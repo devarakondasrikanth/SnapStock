e84b0d3ba52754f61658c5192cbea498b5261373
devsr02 <devsr02@DEVSR02-W7.ca.com>
2016-06-17 15:59:41 +0530

    commiting changes

--- src/com/dao/Queries.java
+++ src/com/dao/Queries.java
@@ -3,7 +3,7 @@ public interface Queries {
 	public String LoginCheck = "from PixUserInfo UI where UI.userId = ? and UI.password = ?";
 	public String viewPhotoQry="from PixPhotoInfo photo where photo.category=?";
 	public String getMaxDlQuery ="select count(*),pix.photoInfo.photoId  from PixTransactions pix  group by photo_id order by col_0_0_ desc";
-	public String getNewArrivals="from PixPhotoInfo where dateOfUpload >= sysdate-1 order by dateOfUpload desc";
+	public String getNewArrivals="from PixPhotoInfo where dateOfUpload >= cudate()-1 order by dateOfUpload desc";
 	public String getUserPix="from PixPhotoInfo info where info.userInfo.userId=?";
 	public String getHistory="from PixTransactions trans where trans.invoice.invoiceId in (select invoiceId from PixInvoice info where info.userInfo.userId=?)";
 	public String getInvoiceById="from PixInvoice inv where user_id=?";
